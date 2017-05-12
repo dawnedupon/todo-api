35d62bc3ed61e466cc7ef538b4b566832f220c92
Dawn Toh <dawn.toh@gmail.com>
2017-05-12 20:12:28 +0800

    Added POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Chew paper',
-  completed: false
-}, {
-  id: 2,
-  description: 'Inspect plants',
-  completed: false
-}, {
-  id: 3,
-  description: 'Show belly to David',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+//anytime a JSON request comes in, express will parse it
+//and we will be able to access it via req.body
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -21,7 +16,7 @@ app.get('/', function(req, res) {
 
 //Get the whole collection
 //GET /todos
-app.get('/todos', function(req,res) {
+app.get('/todos', function(req, res) {
   //convert the string to JSON first, then send it back to whoever called the API
   res.json(todos);
 });
@@ -29,7 +24,7 @@ app.get('/todos', function(req,res) {
 //Get just one model
 // GET /todos/:id
 //Colon is what express uses to parse the data coming in
-app.get('/todos/:id', function(req,res) {
+app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   var matchedTodo;
 
@@ -44,7 +39,19 @@ app.get('/todos/:id', function(req,res) {
   } else {
     res.status(404).send();
   }
+});
 
+// POST /todos
+app.post('/todos', function(req, res) {
+  var body = req.body;
+
+  //Add id field
+  body.id = todoNextId++;
+
+  //Push body into array
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, function() {
