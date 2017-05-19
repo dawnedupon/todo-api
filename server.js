daa0bef0156a231abc78b31ac7bbdcf2fc8c5e91
Dawn Toh <dawn.toh@gmail.com>
2017-05-19 18:21:31 +0800

    Fixed faulty DELETE

--- server.js
+++ server.js
@@ -131,14 +131,21 @@ app.delete('/todos/:id', function(req, res) {
   //   res.json(matchedTodo);
   // }
 
-  db.todo.destroy(body).then(function(todoId) {
+  db.todo.destroy({
     where: {
       id: todoId
     }
+  }).then(function(rowsDeleted) {
+    if (rowsDeleted === 0) {
+      res.status(404).json({
+        error: 'No todo with this id'
+      });
+    } else {
+      res.status(204).send(); //204: Everything went well, nothing to send back
+    }
   }), function(e) {
-    res.status(404).send();
-  }
-
+    res.status(500).send();
+  };
 
 });
 
