d24f6868e6bddf427acb0c23e8b0a2c33d2a41ae
Dawn Toh <dawn.toh@gmail.com>
2017-05-24 16:36:50 +0800

    Converted CRUD request for current user only

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
 
-//Get the whole collection
 //GET /todos
 //GET /todos?completed=true
 //GET /todos?completed=true&q=eat
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if (query.hasOwnProperty('completed') && query.completed === 'true') {
     where.completed = true;
@@ -44,23 +45,6 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
     res.status(500).send();
   });
 
-  //var queryParams = req.query;
-  // var filteredTodos = todos;
-  //
-  // if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-  //   //_.where works even if this is true of more than one item
-  //   filteredTodos = _.where(filteredTodos, {completed: true});
-  // } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-  //   filteredTodos = _.where(filteredTodos, {completed: false});
-  // }
-  //
-  // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-  //   filteredTodos = _.filter(filteredTodos, function(todo) {
-  //     return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-  //   });
-  // }
-  //
-  // res.json(filteredTodos);
 });
 
 //Get just one model
@@ -69,7 +53,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
-  db.todo.findById(todoId).then(function(todo) {
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo) {
       if (!!todo) { //if there is a todo item
         res.json(todo.toJSON());
       } else {
@@ -79,14 +68,6 @@ app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     res.status(500).send(); //500 means server error
   });
 
-  // var matchedTodo = _.findWhere(todos, {
-  //   id: todoId
-  // });
-  // if (matchedTodo) {
-  //   res.json(matchedTodo);
-  // } else {
-  //   res.status(404).send();
-  // }
 });
 
 // POST /todos
@@ -104,42 +85,16 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
     res.status(400).json(e);
   });
 
-  // //if body.completed is not a boolean or if body.description is not a string or there is an empty string
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-  //   return res.status(400).send(); //"bad data"
-  // }
-  //
-  // body.description = body.description.trim();
-  //
-  // //Add id field
-  // body.id = todoNextId++;
-  //
-  // //Push body into array
-  // todos.push(body);
-  //
-  // res.json(body);
-
 });
 
 //DELETE /todos/:id
 app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  // var matchedTodo = _.findWhere(todos, {
-  //   id: todoId
-  // });
-  //
-  // if (!matchedTodo) {
-  //   res.status(404).json({
-  //     "error": "No todo found with that id"
-  //   });
-  // } else {
-  //   todos = _.without(todos, matchedTodo);
-  //   res.json(matchedTodo);
-  // }
 
   db.todo.destroy({
     where: {
-      id: todoId
+      id: todoId,
+      userId: req.user.get('id')
     }
   }).then(function(rowsDeleted) {
     if (rowsDeleted === 0) {
@@ -160,33 +115,6 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   var body = _.pick(req.body, 'description', 'completed');
   var attributes = {};
-  // var matchedTodo = _.findWhere(todos, {
-  //   id: todoId
-  // });
-  // var validAttributes = {}; //Object that stores the attributes we want to update
-
-  // if (!matchedTodo) {
-  //   return res.status(404).send();
-  // }
-
-  // if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-  //   //'completed' must exist and be a boolean
-  //   validAttributes.completed = body.completed;
-  // } else if (body.hasOwnProperty('completed')) {
-  //   //'completed' exists but syntax is bad
-  //   return res.status(400).send();
-  // } //else {
-  //   //Never provided attribute. Continue with request
-  // //}
-  //
-  // if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-  //   validAttributes.description = body.description;
-  // } else if (body.hasOwnProperty('description')) {
-  //   return res.status(400).send();
-  // }
-
-  // _.extend(matchedTodo, validAttributes); //(destination, source) extend copies all the properties in source objects to destination object
-  // res.json(matchedTodo);
 
   if (body.hasOwnProperty('completed')) {
     attributes.completed = body.completed;
@@ -196,7 +124,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     attributes.description = body.description;
   }
 
-  db.todo.findById(todoId).then(function(todo) {
+  db.todo.findOne({
+    where: {
+      id: todoId,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo) {
     if (todo) {
       todo.update(attributes).then(function(todo) {
           res.json(todo.toJSON());
